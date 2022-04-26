b5b41c2184f468b2301a6d301845ea2137407bd8
Abhishek <abhishekr@techis.io>
2022-04-26 02:53:45 -0700

    files

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'django_forum.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
